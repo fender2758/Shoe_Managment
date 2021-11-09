8982b8b00df961964f9d58f2ae49df37c1392aad
fender2758 <56300092+fender2758@users.noreply.github.com>
2021-11-09 18:57:39 +0900

    Add files via upload
    Shoe detection final

---
+++ She_detection_final/object_detection/demo.py
@@ -0,0 +1,102 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import sys
+from matplotlib import pyplot as plt
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+
+# Import utilites
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+# Grab path to current working directory
+print("hello")
+CWD_PATH = os.getcwd()
+print(CWD_PATH)
+# Path to frozen detection graph .pb file, which contains the model that is used
+# for object detection.
+PATH_TO_CKPT = 'shoeDetection/Python inference/frozen_inference_graph.pb'
+
+# Path to label map file
+PATH_TO_LABELS = 'shoeDetection/Python inference/label_map.pbtxt'
+
+# Path to image
+PATH_TO_IMAGE_FOLDER = 'shoeDetection/Python inference/'
+
+# Number of classes the object detector can identify
+NUM_CLASSES = 1
+
+# Load the label map.
+# Label maps map indices to category names, so that when our convolution
+# network predicts `5`, we know that this corresponds to `king`.
+# Here we use internal utility functions, but anything that returns a
+# dictionary mapping integers to appropriate string labels would be fine
+
+print(CWD_PATH)
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+print("end")
+
+# Load the Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+
+# Define input and output tensors (i.e. data) for the object detection classifier
+
+# Input tensor is the image
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+# Output tensors are the detection boxes, scores, and classes
+# Each box represents a part of the image where a particular object was detected
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+# Each score represents level of confidence for each of the objects.
+# The score is shown on the result image, together with the class label.
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+# Number of objects detected
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+# Load image using OpenCV and
+# expand image dimensions to have shape: [1, None, None, 3]
+# i.e. a single-column array, where each item in the column has the pixel RGB value
+
+
+
+for filename in os.listdir(PATH_TO_IMAGE_FOLDER):
+    if filename.endswith(".jpg"): 
+        print(filename)
+        image = cv2.imread(os.path.join(PATH_TO_IMAGE_FOLDER, filename))
+        image_expanded = np.expand_dims(image, axis=0)
+
+        # Perform the actual detection by running the model with the image as input
+        (boxes, scores, classes, num) = sess.run([detection_boxes, detection_scores, detection_classes, num_detections],feed_dict={image_tensor: image_expanded})
+
+        # Draw the results of the detection (aka 'visulaize the results')
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=5,
+            min_score_thresh=0.50)
+        
+        # All the results have been drawn on image. Now display the image.
+        
+        for i, b in enumerate(scores[0]):
+            if b>0.5:
+                print(b)
+                print(boxes[0][i])
+        plt.imshow(image)
+        plt.show()
